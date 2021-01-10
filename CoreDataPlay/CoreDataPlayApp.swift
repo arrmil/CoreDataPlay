3b028518d04b26bf584a5901fb1a61a1268c3893
Arturas Milius <arturas114@gmail.com>
2021-01-10 23:10:06 +0200

    Initial Commit

---
+++ CoreDataPlay/CoreDataPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPlayApp.swift
+//  CoreDataPlay
+//
+//  Created by Arturas Milius on 2021-01-10.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
